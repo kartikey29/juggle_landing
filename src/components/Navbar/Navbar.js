547da8f388be4fb31a2fffe4ebd104313e676d72
kartikey sood <kartikeysood@rediffmail.com>
2022-05-20 14:27:53 +0530

    added navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,14 @@
 import classes from "./Navbar.module.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={classes.Navbar}>
+      <div className={classes.Navholder}>
+        <h2>Juggle</h2>
+        <button>Join Waitlist</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
